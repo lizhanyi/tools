1d54cfe61f2744a11d0931ff7705e66a893e3819
flylee <634874196@qq.com>
2019-07-19 23:47:40 +0800

    新增 Memory 方法

--- src/memory/index.js
+++ src/memory/index.js
@@ -3,47 +3,113 @@
  * @param { type: 存储类型 local => localStorage, session => sessionStorage  }
 */
 export default class Memory{
+
+    /**
+     *Creates an instance of Memory.
+     * @param { 存储的键 } key
+     * @param { 存储类型 } type
+     * @memberof Memory
+     */
     constructor( key, type ){
         this.key = key;
         this.type = type;
+        this._getIntance( type, key );
     }
+
+
+    /**
+     * 存储类型 映射
+     */
     map = {
         'session': window.sessionStorage,
         'local': window.localStorage
     }
+
+    
+    /**
+     * 存储数据， value 数据
+     */
     setItem( value ){
         this.map[ this.type ].setItem( this.key, JSON.stringify( value ) );
     }
+
+
+    /**
+     * 获取 存储的值
+     */
     getItem(){
         if( this.getKey() ){
             return JSON.parse( this.map[ this.type ].getItem( this.key ) );
         }
     }
+
+
+    /**
+     * 删除 单条 存储数据
+     */
     removeItem(){
         this.map[ this.type ].removeItem( this.key )
     }
+
+
+    /**
+     * 检测 存储 是否存在 通过 key， 返回布尔值
+     */
     getKey(){
         return this.key in this.map[ this.type ];
     }
 
+
+    /**
+     * 内部方法
+     * type: 存储类型， key: 数据的键 
+     */
+    _getIntance( type, key ){
+
+        const { instances } = this.constructor;
+
+        const values = instances[ type ] || [];
+
+        if( values.length !== 0 && values.includes( key ) ){
+            throw 'key is not only key, Memory need a only key, please check code'
+        }
+        
+        this.constructor.instances = { 
+            ...instances,
+            [ type ]: [ ...values, key ]
+        };
+    }
+
+    
+    /**
+     * 存储实例, 获取
+     */
+    static instances = {}
+
+
+    /**
+     * 清除空指定类型的数据
+     */
     static clear( type="local"){
         new this().map[ type ].clear()
     }
 
-    // 删除 指定键值得数据
+
+    /**
+     * 批量删除 存储数据
+     */
     static removeItems(){
 
     }
-    
-    // 获取 存储的 key 值
+
+
+    /**
+     * 获取存储 亿存储数据的 key 值
+     */
     static keys(){
-        return {
-            local: [],
-            session: [],
-        }
+        return Object.entries( new this().map ).map( ([ key, value ]) => ({ 
+                [ key ]: new Array( value.length ).fill('').map( (item, index ) => value.key(index) )   
+            })
+        );
     }
 }
-
-
-
-
