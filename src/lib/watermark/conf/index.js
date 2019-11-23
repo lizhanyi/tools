15322ca0aec601e84e382f9676e671a1cacac749
flylee <634874196@qq.com>
2019-11-23 23:08:31 +0800

    新增 水印生成 逻辑

---
+++ src/lib/watermark/conf/index.js
@@ -0,0 +1,35 @@
+/***
+ * 默认 配置项
+ */
+export default {
+
+    /** 
+     * 字体颜色， 支持 rgba
+    */
+    color: '#000',
+
+    /** 
+     * 字体相关设置，复合写法
+    */
+    font: 'normal normal 18px 微软雅黑',
+    
+    /**
+     * 文案背景色， 默认 无
+     */
+    background: 'none',
+
+    /**
+     * 旋转角度
+     */
+    // rotate: 30, 
+
+    /**
+     * 文案信息
+     */
+    content: 'hello world',
+
+    /** 
+     * 模式 2 种, 1, background; 2, canvas; 默认 背景模式
+    */
+    // mode: 'background',
+}
---
+++ src/lib/watermark/index.js
@@ -0,0 +1,125 @@
+/** 
+ * 水印工具类
+ * @dec 通过 canvass 实现 水印效果
+*/
+
+
+import { dom } from './../dom';
+
+import defaultOpt from './conf'; // 默认配置项
+
+export default class Watermark {
+
+    constructor( options ){
+
+        /** 
+         * 合并配置项
+        */
+        this.options = { ...defaultOpt, ...options };
+
+    }
+
+
+    /**
+     * 功能：获取文案边界范围
+     * 参数：无
+     * 返回值：无
+     */
+    getTextBorderline(){
+
+        const { font, content } = this.options;
+        
+        const span = dom.create( 'span' );
+
+        span.innerHTML = content;
+
+        Object.assign( span.style, {
+            font,
+            position: 'absolute',
+            top: '-10000px',
+            left: '-10000px',
+            zIndex: -1,
+        });
+
+        dom.appendChild( span );
+
+        const offset = dom.getOffset( span );
+
+        dom.remove( span );
+
+        return offset
+
+    }
+
+
+    /**
+     * 功能：初始化，创建画布
+     * 参数：无
+     * 返回值：实例对象
+     */
+    init(){
+
+        const canvas = dom.create( 'canvas' );
+
+        Object.assign( canvas, { ...this.getTextBorderline() });
+
+        canvas.style.display = 'none';
+
+        dom.appendChild( canvas );
+
+        this.canvas = canvas;
+
+        return this;
+
+    }
+
+
+    /**
+     * 功能：渲染画布
+     * 参数：无
+     * 返回值：
+     */
+    render(){
+        
+        const { options: { font, color, content }, canvas } = this;
+        const ctx = canvas.getContext( '2d' );
+
+        ctx.save();
+        ctx.font = font;
+        ctx.fillStyle = color;
+        ctx.textBaseline = 'bottom';
+        ctx.fillText( content, 0, canvas.height );
+        ctx.restore();
+        return this;
+
+    }
+
+
+    /**
+     * 功能：获取图片
+     * 参数：needle, 是否直接获取图片， 默认不直接获取
+     * 返回值：无
+     */
+    getImage( needle=false ){
+
+        needle && this.init().render();
+
+        const image = this.canvas.toDataURL( 'image/png' );
+
+        dom.remove( this.canvas );
+
+        return image;
+    }
+
+
+    /**
+     * 功能：从新渲染画布
+     * 参数： content, 内容
+     * 返回值：实例对象
+     */
+    reRender( content="hello world" ){
+        this.options = { ...this.options, content };
+        this.init().render();
+    }
+
+}
